fe1e0a2173bc654fcd7e0668b470a799ca2dcf8c
hiroboxm-lgtm <hirobox.m@gmail.com>
2026-04-14 22:14:10 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,39 @@
-const CACHE_NAME = "inspection-app-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+var CACHE_NAME = 'jutaku-shindan-v2';
+var FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
 ];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+ 
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+  self.skipWaiting();
+});
+ 
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE_NAME; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+ 
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
   );
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
+ 
